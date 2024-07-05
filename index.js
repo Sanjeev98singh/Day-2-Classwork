671bfc6336ab92d320d2f02a25a62ebc62a3e3a9
Sanjeev <coolviccy123@gmail.com>
2024-07-05 19:15:30 +0530

    page added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function Card(){
     return(
         <div className="card">
-            <h1>Hii My Name is Pritiranjan</h1>
+            <h1>Hii My Name is Sanjeev Singh Bhadouria</h1>
             <div>
                 <p>
-                    I am a Web Developer and I am currently working on my own projects. I am also aStudent at geekster
+                    I am a Web Developer and I am currently working on my own projects. I am also a Student at geekster
                 </p>
             </div>
             <div>
-                <a href ="https://www.linkedin.com/in/pritiranjan-patra-771a91287/">
+                <a href ="https://www.linkedin.com/in/sanjeev-singh-31870624b/">
                 Click Me
                 </a>
             </div>
